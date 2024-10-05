2e2c45e40d8cd36323aaa870ee06ad584d13abc2
panakir <panakir.webdev@gmail.com>
2024-10-05 15:18:10 +0300

    feat: solving cli block tasks

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
+import { stdout } from 'process';
+
 const parseEnv = () => {
-    // Write your code here 
+    let result = '';
+    const prefix = 'RSS_';
+    const keys = Object.keys(process.env);
+
+    keys.filter(key => key.startsWith(prefix)).forEach(key => {
+       result += `${key}=${process.env[key]}\n`;
+    });
+    
+    stdout.write(result);
 };
 
 parseEnv();
