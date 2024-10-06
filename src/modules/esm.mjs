a852b5c702da54ec363f1fca030da5dfb71491a6
panakir <panakir.webdev@gmail.com>
2024-10-06 14:37:59 +0300

    feat: solving modules  task

---
+++ src/modules/esm.mjs
@@ -0,0 +1,35 @@
+import path  from 'path';
+import  { release, version } from 'os';
+import  { createServer as createServerHttp } from 'http';
+import ('./files/c.js');
+import greaterThan from './files/a.json' with {type: 'json'};
+import lessThan from './files/b.json' with {type: 'json'};
+
+const random = Math.random();
+
+const  unknownObject = random > 0.5 ? greaterThan : lessThan;
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${path.sep}"`);
+console.log(`Path to current file is ${import.meta.filename}`);
+console.log(`Path to current directory is ${import.meta.dirname}`);
+
+const myServer = createServerHttp((_, res) => {
+    res.end('Request accepted');
+});
+
+const PORT = 3000;
+
+console.log(unknownObject);
+
+myServer.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+    console.log('To terminate it, use Ctrl+C combination');
+});
+
+export  {
+    unknownObject,
+    myServer,
+};
+
