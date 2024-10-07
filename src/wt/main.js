99b226f4cc58c938d2b995b92933b6ec7a07d966
panakir <panakir.webdev@gmail.com>
2024-10-07 20:05:37 +0300

    feat: solving worker threads tasks

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,28 @@
+import { cpus }from 'os'
+import { Worker } from 'worker_threads';
+import { FIBONACCI_START_NUMBER } from '../../helpers/constants.js';
+
+const createWorker = ({file, data, resolve, reject}) => {
+    return new Worker(file, { workerData: data })
+    .on('message', resolve)
+    .on('error', reject)
+}
+
 const performCalculations = async () => {
-    // Write your code here
+    const file = `${import.meta.dirname}/worker.js` 
+    const workersAmount = cpus().length
+   return Promise.all(new Array(workersAmount)
+   .fill(0)
+   .map((_ , index) => {
+        const {resolve, reject,  promise } = Promise.withResolvers()
+        createWorker({file, 
+            data:FIBONACCI_START_NUMBER + index,
+            resolve, 
+            reject
+         } )
+        return promise
+    })).then(res => {console.log(res)})
+
 };
 
 await performCalculations();
