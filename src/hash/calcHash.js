88fca133edc2c498259814fc3b80a7703b8ad4d9
panakir <panakir.webdev@gmail.com>
2024-10-07 12:25:40 +0300

    feat: solving hash  task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import { getFilePath } from "../../helpers/getFilePath.js";
+import crypto from 'crypto';
+import fs from 'fs';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const file = getFilePath('fileToCalculateHashFor.txt', import.meta.url);
+    const hash = crypto.createHash('sha256');
+    const stream = fs.createReadStream(file);
+    
+    stream.on('data', (data) => hash.update(data));
+    stream.on('end', () => {
+        const fileHash = hash.digest('hex');
+        process.stdout.write(`SHA256 hash for ${file} is ${fileHash}`);
+    });  
 };
 
 await calculateHash();
