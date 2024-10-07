27245f09817edc3204323cf1561bcd5fcc6e1297
panakir <panakir.webdev@gmail.com>
2024-10-07 13:05:33 +0300

    feat: solving stream tasks

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,17 @@
+import {stdin, stdout} from 'process';
+import {Transform} from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+
+    const reverseTransform = new Transform({
+        transform(chunk, _encoding, callback) {
+            const reversedChunk = chunk.toString().split('').reverse().join('');
+            callback(null, reversedChunk);
+        }
+    });
+    stdout.write('For exit press "ctrl + c"! \n');
+    stdout.write('Type your text here:  \n');
+    stdin.pipe(reverseTransform).pipe(stdout);
 };
 
 await transform();
